97c44d1d264897abff5b8cb588620e5b01e05157
Elia Brown <eliabrown@Elias-MacBook.local>
2021-11-16 11:54:58 -0700

    will come back

--- public/service.worker.js
+++ public/service.worker.js
@@ -1,66 +1,66 @@
-const APP_PREFIX = 'MaestroBistro-';     
+const APP_PREFIX = 'MaestroBistro-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-    "./index.html",
-    "./js/idb.js",
-    "./js/index.js",
-    "./manifest.json",
-    "./css/styles.css",
-    "./icons/icon-72x72.png",
-    "./icons/icon-96x96.png",
-    "./icons/icon-128x128.png",
-    "./icons/icon-144x144.png",
-    "./icons/icon-152x152.png",
-    "./icons/icon-192x192.png",
-    "./icons/icon-384x384.png",
-    "./icons/icon-512x512.png"
-  ];
+  "./index.html",
+  "./js/idb.js",
+  "./js/index.js",
+  "./manifest.json",
+  "./css/styles.css",
+  "./icons/icon-72x72.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-512x512.png"
+];
 
-self.addEventListener('install', function (e) {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-          console.log('installing cache : ' + CACHE_NAME)
-          return cache.addAll(FILES_TO_CACHE)
-        })
-      )
+self.addEventListener('install', function (evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('installing cache : ' + CACHE_NAME);
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', function(e) {
-    e.waitUntil(
-      caches.keys().then(function(keyList) {
-        let cacheKeeplist = keyList.filter(function(key) {
-          return key.indexOf(APP_PREFIX);
-        });
-        cacheKeeplist.push(CACHE_NAME);
-  
-        return Promise.all(
-          keyList.map(function(key, i) {
-            if (cacheKeeplist.indexOf(key) === -1) {
-              console.log('deleting cache : ' + keyList[i]);
-              return caches.delete(keyList[i]);
-            }
-          })
-        );
-      })
-    );
-  });
 
-  self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-      caches.match(e.request).then(function (request) {
-        if (request) { // if cache is available, respond with cache
-          console.log('responding with cache : ' + e.request.url)
-          return request
-        } else {       // if there are no cache, try fetching request
-          console.log('file is not cached, fetching : ' + e.request.url)
-          return fetch(e.request)
-        }
-  
-        // You can omit if/else for console.log & put one line below like this too.
-        // return request || fetch(e.request)
-      })
-    )
-  });
+self.addEventListener('activate', function (evt) {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== APP_PREFIX) {
+            console.log('deleting cache :', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function (evt) {
+  console.log('fetch request : ' + evt.request.url)
+  evt.respondWith(
+    caches.match(evt.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + evt.request.url)
+        return request
+      } else {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + evt.request.url)
+        return fetch(evt.request)
+      }
+
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
+    })
+  )
+});
